4dfb6cc82ffb25113f825f5493089065eb1262b7
square1grp-dev <developer01@square1grp.com>
2020-02-13 00:37:28 -0800

    clean up js files

--- wrw/static/js/update-factors.js
+++ wrw/static/js/update-factors.js
@@ -1,48 +1,3 @@
-var getTrHTML = function (factorID, factorTitle, factorLevels, is_daily_factor) {
-    template = `
-        <tr>
-            <td class="align-middle" width="200px">
-                <h5 class="mb-0">FactorTitle</h5>
-                <input type="hidden" name="factor_`+ (is_daily_factor ? "Daily" : "Intermittent") + `_IDs" value="FactorID"/>
-            </td>
-
-            <td class="align-middle" width="500px">
-                <div class="row mx-auto">
-    `;
-
-    for (i = 0; i < factorLevels.length - (is_daily_factor ? 0 : 1); i++) {
-        template += `
-            <div class="col text-center">
-                <label for="radio_FactorID_`+ (i + 1) + `" class="text-center w-100">` + factorLevels[i] + `</label>
-                <input id="radio_FactorID_`+ (i + 1) + `" type="radio" name="factor_FactorID_level" value="` + (i + 1) + `"/>
-            </div>
-        `;
-    }
-
-    template += `
-                </div>
-            </td>
-
-            <td class="align-middle" width="200px">
-                <input name="factor_FactorID_description" class="form-control" placeholder="Description">
-            </td>
-
-            <td class="align-middle">
-                <div class="row mx-auto">
-                    <button id="convert-factor-FactorID" class="btn btn-primary mr-2">Make ` + (is_daily_factor ? `Intermittent` : "Daily") + `</button>`;
-
-    template += `
-                    <button class="btn btn-danger delete" id="delete-factor-FactorID" type="button" data-factor-id="FactorID">
-                        <i class="fa fa-times" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </td>
-        </tr>
-    `;
-
-    return template.replace(/FactorTitle/gi, factorTitle).replace(/FactorID/gi, factorID);
-};
-
 $(document).ready(function () {
     var showAlert = function (msg) {
         $(".alert strong").text(msg);
@@ -53,66 +8,6 @@ $(document).ready(function () {
         }, 500);
 
     };
-
-    var addFactor = function (factor_id, is_daily_factor) {
-        if (factor_id) {
-            $.each(org_factors, function (idx, factor) {
-                if (factor["id"] == factor_id) {
-                    var template = getTrHTML(factor["id"], factor["title"], factor["levels"], is_daily_factor);
-
-                    if (is_daily_factor) {
-                        $("form.uf-form table.cdf-list tbody").append(template);
-                    } else {
-                        $("form.uf-form table.cif-list tbody").append(template);
-                    }
-
-                    $("form.uf-form tbody tr input[type=radio], form.uf-form tbody tr input[type=checkbox]").unbind().change(function () {
-                        showAlert("Data Modified");
-                    });
-
-                    $("form.uf-form select.factors option[value=" + factor["id"] + "]").remove();
-                    $("form.uf-form select.factors").val("-1");
-
-                    $("form.uf-form button#convert-factor-" + factor["id"]).click(function () {
-                        $(this).parents("tr").remove()
-
-                        if (is_daily_factor)
-                            convertToIntermittentFactor(factor["id"]);
-                        else
-                            convertToDailyFactor(factor["id"]);
-                    });
-
-                    $("form.uf-form button#delete-factor-" + factor["id"]).click(function () {
-                        $(this).parents("tr").remove();
-
-                        $("form.uf-form select.factors").append("<option value=" + factor["id"] + ">" + factor["title"] + "</option>");
-                    });
-                }
-            });
-        }
-    };
-
-    var convertToIntermittentFactor = function (factor_id) {
-        addFactor(factor_id, false);
-    };
-
-    var convertToDailyFactor = function (factor_id) {
-        addFactor(factor_id, true);
-    };
-
-    $("form.uf-form button.convert-to-daily").click(function () {
-        $(this).parents("tr").remove()
-
-        var factor_id = $(this).data("factor-id");
-        convertToDailyFactor(factor_id);
-    });
-
-    $("form.uf-form button.convert-to-intermittent").click(function () {
-        $(this).parents("tr").remove()
-
-        var factor_id = $(this).data("factor-id");
-        convertToIntermittentFactor(factor_id);
-    });
 
     $("form.uf-form button.delete:not(.end-daily-factor)").click(function () {
         var factor_id = $(this).data("factor-id");
@@ -127,45 +22,30 @@ $(document).ready(function () {
     $("form.uf-form button#add-intermittent-factor").click(function () {
         var factor_id = $("select#a-intermittent-factors").val();
 
-        if (factor_id) {
-            if ($(this).hasClass("cif")) {
-
-                if (factor_id > 0) {
-                    window.location.href = "/user/" + user_id + "/update_factors/?action=add_cif&factor_id=" + factor_id;
-                }
-            } else {
-                addFactor(factor_id, false)
-            }
+        if (factor_id > 0 && $(this).hasClass("cif")) {
+            window.location.href = "/user/" + user_id + "/update_factors/?action=add_cif&factor_id=" + factor_id;
         }
     });
 
     $("form.uf-form button#add-daily-factor").click(function () {
         var factor_id = $("select#a-daily-factors").val();
 
-        if (factor_id) {
-            if ($(this).hasClass("udfs")) {
-                var date = $("#date").val();
-                var time = $("#time").val();
+        if (factor_id > 0 && $(this).hasClass("udfs")) {
+            var date = $("#date").val();
+            var time = $("#time").val();
 
-                if (factor_id > 0) {
-                    window.location.href = "/user/" + user_id + "/update_factors/?action=add_udfs&factor_id=" + factor_id + "&date=" + date + "&time=" + time;
-                }
-            } else {
-                addFactor(factor_id, true)
-            }
+            window.location.href = "/user/" + user_id + "/update_factors/?action=add_udfs&factor_id=" + factor_id + "&date=" + date + "&time=" + time;
         }
     });
 
     $("form.uf-form button.end-daily-factor").click(function () {
         var udfs_id = $(this).data("udfs-id");
 
-        if (udfs_id && $(this).hasClass("udfe")) {
+        if (udfs_id > 0 && $(this).hasClass("udfe")) {
             var date = $("#date").val();
             var time = $("#time").val();
 
-            if (udfs_id > 0) {
-                window.location.href = "/user/" + user_id + "/update_factors/?action=add_udfe&udfs_id=" + udfs_id + "&date=" + date + "&time=" + time;
-            }
+            window.location.href = "/user/" + user_id + "/update_factors/?action=add_udfe&udfs_id=" + udfs_id + "&date=" + date + "&time=" + time;
         }
     });
 
--- wrw/static/js/update-symptom-severities.js
+++ wrw/static/js/update-symptom-severities.js
@@ -1,45 +1,3 @@
-var getTrHTML = function (symptomID, symptomName, symptomLevels) {
-    template = `
-        <tr>
-            <td class="align-middle" width="250px">
-                <h5 class="mb-0">SymptomName</h5>
-                <input type="hidden" name="symptom_IDs" value="SymptomID"/>
-            </td>
-
-            <td class="align-middle" width="550px">
-                <div class="row mx-auto">
-    `;
-
-    for (i = 0; i < symptomLevels.length; i++) {
-        template += `
-            <div class="col text-center">
-                <label for="radio_SymptomID_`+ (i + 1) + `" class="text-center w-100">` + symptomLevels[i] + `</label>
-                <input id="radio_SymptomID_`+ (i + 1) + `" type="radio" name="symptom_SymptomID_level" value="` + (i + 1) + `"/>
-            </div>
-        `;
-    }
-
-    template += `
-                </div>
-            </td>
-
-            <td class="align-middle" width="250px">
-                <input name="symptom_SymptomID_description" class="form-control" placeholder="Description">
-            </td>
-
-            <td class="align-middle">
-                <div class="row mx-auto">
-                    <button class="btn btn-danger delete" id="delete-symptom-SymptomID" type="button" data-symptom-id="SymptomID">
-                        <i class="fa fa-times" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </td>
-        </tr>
-    `;
-
-    return template.replace(/SymptomName/gi, symptomName).replace(/SymptomID/gi, symptomID);
-};
-
 $(document).ready(function () {
     var showAlert = function (msg) {
         $(".alert strong").text(msg);
@@ -65,33 +23,8 @@ $(document).ready(function () {
     $("form.uss-form button#add-symptom").click(function () {
         var symptom_id = $("select.symptoms").val();
 
-        if (symptom_id) {
-            if ($(this).hasClass("cus")) {
-                if (symptom_id > 0) {
-                    window.location.href = "/user/" + user_id + "/update_symptom_severities/?action=add_cus&symptom_id=" + symptom_id;
-                }
-            } else {
-                $.each(org_symptoms, function (idx, symptom) {
-                    if (symptom["id"] == symptom_id) {
-                        var template = getTrHTML(symptom["id"], symptom["name"], symptom["levels"]);
-
-                        $("form.uss-form table.cus-list tbody").append(template);
-
-                        $("form.uss-form button#delete-symptom-" + symptom["id"]).click(function () {
-                            $(this).parents("tr").remove()
-
-                            $("form.uss-form select.symptoms").append("<option value=" + symptom["id"] + ">" + symptom["name"] + "</option>");
-                        });
-
-                        $("form.uss-form tbody tr input[type=radio]").unbind().change(function () {
-                            showAlert("Data Modified");
-                        });
-
-                        $("form.uss-form select.symptoms option[value=" + symptom["id"] + "]").remove()
-                        $("form.uss-form select.symptoms").val("-1");
-                    }
-                });
-            }
+        if (symptom_id > 0 && $(this).hasClass("cus")) {
+            window.location.href = "/user/" + user_id + "/update_symptom_severities/?action=add_cus&symptom_id=" + symptom_id;
         }
     });
 
